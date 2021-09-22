a7658e3ce5d180cc8d94ca003343af2d46e7cb58
David Casey <david.casey.work@gmail.com>
2021-09-21 23:56:50 -0400

    Ab#39:  Backend JWT Verification and OpenAPI Docs (#3)
    * AB#39: adds auth0 jwt verification and openapi documentation
    
    * AB#39: adds tests for audience validator
    
    * AB#39: fixes sonar exclusion for configs
    
    * AB#39: fixes sonar exclusions
    
    Co-authored-by: David Casey <caseyj@Davids-MBP.localdomain>

---
+++ src/test/java/com/psu/devboards/dbapi/utils/AudienceValidatorTest.java
@@ -0,0 +1,41 @@
+package com.psu.devboards.dbapi.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.time.Instant;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+class AudienceValidatorTest {
+
+    AudienceValidator audienceValidator;
+
+    @BeforeEach
+    void setUp() {
+        audienceValidator = new AudienceValidator("test");
+    }
+
+    @Test
+    void shouldReturnSuccessForCorrectAudience() {
+        Jwt jwt = new Jwt("a", Instant.MIN, Instant.now(), Collections.singletonMap("foo", "bar"),
+                Collections.singletonMap("aud", "test"));
+
+        OAuth2TokenValidatorResult result = audienceValidator.validate(jwt);
+        assertFalse(result.hasErrors());
+    }
+
+    @Test
+    void shouldReturnFailureForIncorrectAudience() {
+        Jwt jwt = new Jwt("a", Instant.MIN, Instant.now(), Collections.singletonMap("foo", "bar"),
+                Collections.singletonMap("aud", "wrong"));
+
+        OAuth2TokenValidatorResult result = audienceValidator.validate(jwt);
+        assertTrue(result.hasErrors());
+    }
+}
