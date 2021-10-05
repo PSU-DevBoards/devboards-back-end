74e1ce2fd635ab0675d5dadc521498f06d39058a
KappaJ <masonticehurst@me.com>
2021-10-05 01:24:09 -0400

    Add base user controller

---
+++ src/main/java/com/psu/devboards/dbapi/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.psu.devboards.dbapi.controllers;
+
+import com.psu.devboards.dbapi.models.entities.User;
+import com.psu.devboards.dbapi.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public User getMe(Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        return user;
+    }
+}
