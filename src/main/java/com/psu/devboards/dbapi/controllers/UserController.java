c4d68de89df6557f636d2ef205207b08b41594cb
kpjVideo <masonticehurst@me.com>
2021-10-05 17:55:10 -0400

    AB#62: Add base user controller (#8)
    * Add base user controller
    
    * Code smells
    
    * Pluralize request mapping
    
    * tests to reflect plural

---
+++ src/main/java/com/psu/devboards/dbapi/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.psu.devboards.dbapi.controllers;
+
+import com.psu.devboards.dbapi.models.entities.User;
+import com.psu.devboards.dbapi.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public User getMe(Principal principal){
+        return userService.findByUsername(principal.getName());
+    }
+}
